30f60af8b9785590885c0e8318523b47a7706c3e
Daniela <danielavitosova@seznam.cz>
2019-10-21 16:48:10 +0200

    Adding welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # to be changed
-  root "home#index"
+  # welcome page
+  root "pages#home"
+
 
 end
